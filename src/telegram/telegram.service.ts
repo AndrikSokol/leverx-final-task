a56e37a164835abe939ae2437e6a7b1369d988fd
AndrikSokol <AndrikSokol31@gmail.com>
2024-05-13 15:06:12 +0300

    fix: can send vinyl without image

--- src/telegram/telegram.service.ts
+++ src/telegram/telegram.service.ts
@@ -5,6 +5,7 @@ import { TELEGRAM_MODULE_OPTIONS } from './telegram.constants';
 import { MessageDto } from './dto/message.dto';
 import { readFile } from 'fs-extra';
 import { ConfigService } from '@nestjs/config';
+import { ParseMode } from 'telegraf/typings/core/types/typegram';
 
 @Injectable()
 export class TelegramService {
@@ -24,6 +25,13 @@ export class TelegramService {
       `<b>Name:</b> ${messageDto.name}\n` +
       `<a href=\"${this.configService.get('BASE_URL')}/vinyl/${messageDto.link}\">Link</a>\n` +
       `<b>Price:</b> ${messageDto.price}$`;
+
+    if (messageDto.image === null) {
+      await this.bot.telegram.sendMessage(this.options.chatId, message, {
+        parse_mode: 'HTML' as ParseMode,
+      });
+      return;
+    }
 
     const imageFilePath = `uploads/${messageDto.image}`;
 
