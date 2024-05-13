5c59fe9ac23c143c4108ce8dac92aae2ec556ee1
AndrikSokol <AndrikSokol31@gmail.com>
2024-05-13 11:07:59 +0300

    fix: check to file

--- src/utils/isExistsFIle.ts
+++ src/utils/isExistsFIle.ts
@@ -3,6 +3,10 @@ import { pathExists } from 'fs-extra';
 import * as path from 'path';
 
 export const isExistsFile = async (filename: string): Promise<boolean> => {
+  if (filename === undefined || filename === null) {
+    return true;
+  }
+
   const filePath = path.join(appPath, 'uploads', filename);
 
   return await pathExists(filePath);
